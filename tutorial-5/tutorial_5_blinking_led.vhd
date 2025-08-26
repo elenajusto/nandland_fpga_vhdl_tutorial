83b3ba4a57b74f49d42d4dc55b6f6b29ff860168
Elena Justo <elena11j@protonmail.ch>
2025-08-26 15:02:01 +1000

    Placeholder files for tut5, also update notes

---
+++ tutorial-5/tutorial_5_blinking_led.vhd
@@ -0,0 +1,44 @@
+-- File: Top Level
+
+-- Libraries
+library ieee;
+use ieee.std_logic_1164.all;
+
+--
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- TODO: Remove placeholder
+-- Definition: AND Gate
+
+-- Interface 
+-- This defines the and_gate IO
+entity and_gate_new is
+    port (
+        input_1   : in std_logic;
+        input_2 : in std_logic;
+        result : out std_logic
+    );
+end entity;
+
+-- Functionality
+-- This is an architecture called "rtl" of type "and_gate" 
+architecture rtl of and_gate_new is
+    signal andGate : std_logic;
+begin
+    andGate <= input_1 and input_2;
+    result <= andGate;
+end architecture;
