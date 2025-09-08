d555fc8825e14056b2a7fc09c2197f0e2a78d870
Elena Justo <elena11j@protonmail.ch>
2025-09-09 09:24:36 +1000

    Moved stuff into folders so i can testbench and simulate my example codes

---
+++ free_range_exercises/chapter-4/ch4-7/chapter_4-7.vhd
@@ -0,0 +1,16 @@
+-- Libraries
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- Chapter 4 - Example 7
+-- Write the VHDL code that implements the following  circuit. 
+-- The circuit contains an input bundle of four signals and
+-- an output bundle of three signals. 
+-- The input bundle, D IN, represents a 4bit binary number. 
+-- The output bundle, SZ OUT, is used to indicate the magnitude 
+-- of the 4-bit binary input number. 
+
+-- The relationship between  the input and output is shown in 
+-- the table below. 
+
+-- Use a selected signal  assignment statement in the solution.
